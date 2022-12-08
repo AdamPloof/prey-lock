68d24acfea68d741c471dfcbfe898838ceaacc42
Adam Ploof <adamploof@hotmail.com>
2022-12-08 00:32:47 -0500

    restrict resizing to boundries of the canvas

--- interface/detection_zone_monitor.py
+++ interface/detection_zone_monitor.py
@@ -1,14 +1,19 @@
 from tkinter import *
 from tkinter import ttk
 
+"""
+TODO: Move detection zone around with drag.
+Add tag to all items of detection zone to make moving the whole thing around easier.
+"""
+
 class DetectionZoneMonitor:
-    # The smallest the window can be resized to for each axis
-    RESIZE_MIN_THRESHOLD = 50
-    # The radius of the resize controls
-    RESIZE_CNTRL_RAD = 10
+    
+    RESIZE_MIN_THRESHOLD = 50 # The smallest the window can be resized to for each axis
+    RESIZE_CNTRL_RAD = 10 # The radius of the resize controls
+    DETECTION_ZONE_TAG = 'dzone'
     
     def __init__(self, container) -> None:
-        self.canvas = Canvas(container)
+        self.canvas = Canvas(container, background='#e8e9eb')
         self.canvas.grid(column=0, row=0, columnspan=4, rowspan=4, sticky=(N, S, E, W), pady=(0, 12))
 
         self.resize_mousedown = False
@@ -45,18 +50,20 @@ class DetectionZoneMonitor:
 
     def draw_resize_controls(self):
         coords = self.canvas.coords(self.detection_zone)
-
         top = (((coords[0] + coords[2]) / 2) - self.RESIZE_CNTRL_RAD, coords[1] - self.RESIZE_CNTRL_RAD, ((coords[0] + coords[2]) / 2) + self.RESIZE_CNTRL_RAD, coords[1] + self.RESIZE_CNTRL_RAD)
-        self.resize_controls['top'] = self.canvas.create_oval(*top, fill='blue', outline='gray')
-
         bottom = (((coords[0] + coords[2]) / 2) - self.RESIZE_CNTRL_RAD, coords[3] + self.RESIZE_CNTRL_RAD, ((coords[0] + coords[2]) / 2) + self.RESIZE_CNTRL_RAD, coords[3] - self.RESIZE_CNTRL_RAD)
-        self.resize_controls['bottom'] = self.canvas.create_oval(*bottom, fill='blue', outline='gray')
-
         left = (coords[0] + self.RESIZE_CNTRL_RAD, ((coords[1] + coords[3]) / 2) + self.RESIZE_CNTRL_RAD, coords[0] - self.RESIZE_CNTRL_RAD, ((coords[1] + coords[3]) / 2) - self.RESIZE_CNTRL_RAD)
-        self.resize_controls['left'] = self.canvas.create_oval(*left, fill='blue', outline='gray')
-
         right = (coords[2] + self.RESIZE_CNTRL_RAD, ((coords[1] + coords[3]) / 2) + self.RESIZE_CNTRL_RAD, coords[2] - self.RESIZE_CNTRL_RAD, ((coords[1] + coords[3]) / 2) - self.RESIZE_CNTRL_RAD)
-        self.resize_controls['right'] = self.canvas.create_oval(*right, fill='blue', outline='gray')
+
+        cntrls = {
+            'top': top,
+            'bottom': bottom,
+            'left': left,
+            'right': right,
+        }
+
+        for k, pos in cntrls.items():
+            self.resize_controls[k] = self.canvas.create_oval(*pos, fill='blue', outline='gray', tags=(self.DETECTION_ZONE_TAG,))
 
     def listen_for_resize(self):
         for cntrl in self.resize_controls.values():
@@ -68,9 +75,10 @@ class DetectionZoneMonitor:
 
             self.canvas.tag_bind(cntrl, '<B1-Motion>', resize_handler)
         
+    # TODO: Change most of this to an init dzone func and then call it if it hasn't been activated yet, otherwise show dzone.
     def activate(self):
         dzone_coodinates = self.detection_zone_coordinates()
-        self.detection_zone = self.canvas.create_rectangle(*dzone_coodinates, fill='gray', outline='black')
+        self.detection_zone = self.canvas.create_rectangle(*dzone_coodinates, fill='gray', outline='black', tags=(self.DETECTION_ZONE_TAG,))
         self.draw_resize_controls()
         self.listen_for_resize()
 
@@ -112,6 +120,7 @@ class DetectionZoneMonitor:
             self.resize_x(amt, cntrl)
             self.drag_start_x = e.x
 
+    # If we wanted to, the resize_x and resize_y could be refactored into a single method. But it's fine for now.
     def resize_x(self, amt, cntrl):
         new_coords = list(self.canvas.coords(self.detection_zone))
         if self.resize_controls['left'] == cntrl:
@@ -122,6 +131,15 @@ class DetectionZoneMonitor:
         # Prevent user from resizing beyond the min threshold size
         if (new_coords[2] - new_coords[0]) < self.RESIZE_MIN_THRESHOLD:
             return
+
+        # Restrict max resizing to the canvas boundries
+        canvas_bound = self.canvas_boundries()
+        if new_coords[0] < canvas_bound[0]:
+            new_coords[0] = canvas_bound[0]
+            amt = canvas_bound[0] - self.canvas.coords(cntrl)[0] - 10
+        elif new_coords[2] > canvas_bound[2]:
+            new_coords[2] = canvas_bound[2]
+            amt = canvas_bound[2] - self.canvas.coords(cntrl)[2] + 10
 
         self.canvas.coords(self.detection_zone, *new_coords)
         self.canvas.move(cntrl, amt, 0)
@@ -137,6 +155,15 @@ class DetectionZoneMonitor:
         # Prevent user from resizing beyond the min threshold size
         if (new_coords[3] - new_coords[1]) < self.RESIZE_MIN_THRESHOLD:
             return
+
+        # Restrict max resizing to the canvas boundries
+        canvas_bound = self.canvas_boundries()
+        if new_coords[1] < canvas_bound[1]:
+            new_coords[1] = canvas_bound[1]
+            amt = canvas_bound[1] - self.canvas.coords(cntrl)[1] - 10
+        elif new_coords[3] > canvas_bound[3]:
+            new_coords[3] = canvas_bound[3]
+            amt = canvas_bound[3] - self.canvas.coords(cntrl)[3] + 10
 
         self.canvas.coords(self.detection_zone, *new_coords)
         self.canvas.move(cntrl, 0, amt)
@@ -183,4 +210,12 @@ class DetectionZoneMonitor:
             right_y_coords[2] = center_coords[2] + self.RESIZE_CNTRL_RAD
             right_y_coords[3] = center_coords[3] + self.RESIZE_CNTRL_RAD
             self.canvas.coords(self.resize_controls['right'], *right_y_coords)
+
+    def canvas_boundries(self):
+        left = 0
+        top = 0
+        right = left + self.canvas.winfo_width()
+        bottom = top + self.canvas.winfo_height()
+
+        return (left, top, right, bottom)
 
